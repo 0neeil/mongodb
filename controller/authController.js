32049b00b77771d9fa5e94e491a259d56ae99d7a
Dmitry <d.lysenko0405@gmail.com>
2023-03-16 20:39:50 +0200

    Add roles

--- controller/authController.js
+++ controller/authController.js
@@ -1,3 +1,6 @@
+const User = require('../models/User')
+const Role = require('../models/Role')
+
 class authController {
     async registration(req, res){
         try {
@@ -17,6 +20,12 @@ class authController {
 
     async getUsers(req, res){
         try {
+            const userRole = new Role()
+            const adminRole = new Role({value: 'Admin'})
+            
+            await adminRole.save()
+            await userRole.save()
+            
             res.json("work")
         } catch (error) {
             console.log(error)
