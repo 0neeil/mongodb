5cae2ef505544c91e1623cd2fc1e84db5fd3f665
Dmitry <d.lysenko0405@gmail.com>
2023-03-22 16:02:11 +0200

    user registration and validation

--- controller/authController.js
+++ controller/authController.js
@@ -1,12 +1,32 @@
 const User = require('../models/User')
 const Role = require('../models/Role')
+const bcrypt = require('bcryptjs')
+const {validationResult} = require('express-validator')
 
 class authController {
     async registration(req, res){
         try {
+            const errors = validationResult(req)
+            if(!errors.isEmpty()){
+                return res.status(400).json({message:"Registration error", errors})
+            }
+            const {username, password} = req.body
+            const newUser = await User.findOne({username}) 
             
+            if (newUser){
+                return res.status(400).json({message: 'A user with this nickname already exists'})
+            }
+            
+            let hashPassword = bcrypt.hashSync(password, 7)
+            const userRole = await Role.findOne({value: "User"})
+            const user = new User({username, password: hashPassword, roles: [userRole.value]})
+            
+            await user.save()
+            
+            return res.json({message: 'User is now registered'})
         } catch (error) {
-           console.log(error) 
+           console.log(error)
+           res.status(400).json({message: 'Registation error'})
         }
     }
 
@@ -14,18 +34,13 @@ class authController {
         try {
             
         } catch (error) {
-           console.log(error) 
+           console.log(error)
+           res.status(400).json({message: 'Login error'})
         }
     }
 
     async getUsers(req, res){
         try {
-            const userRole = new Role()
-            const adminRole = new Role({value: 'Admin'})
-            
-            await adminRole.save()
-            await userRole.save()
-            
             res.json("work")
         } catch (error) {
             console.log(error)
--- router/authRouter.js
+++ router/authRouter.js
@@ -1,9 +1,16 @@
 const Router = require('express')
 const controller = require('../controller/authController')
+const {check} = require('express-validator')
+
 
 const router = new Router()
 
-router.post('/registration', controller.registration)
+router.post('/registration',[
+    check('username', "This field cannot be empty").notEmpty(),
+    check('username', "The username cannot be shorter than 4 or more than 12 characters").isLength({min: 4, max: 12}),
+    check('password', "This field cannot be empty").notEmpty(),
+    check('password', "The password cannot be shorter than 4 or more than 24 characters").isLength({min: 4, max: 24}) 
+    ], controller.registration)
 router.post('/login', controller.login)
 router.get('/users', controller.getUsers)
 
